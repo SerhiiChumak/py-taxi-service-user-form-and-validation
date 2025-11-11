cbf13d01d2e0db8d84118746f4ba60b036732b19
Serhii Chumak <s4umak@gmail.com>
2025-11-11 18:54:17 +0200

    Solution 3 py taxi service user form and validation

--- taxi/views.py
+++ taxi/views.py
@@ -87,6 +87,7 @@ class CarDetailView(LoginRequiredMixin, generic.DetailView):
             self.object.drivers.remove(user)
         return redirect("taxi:car-detail", pk=self.object.pk)
 
+
 class CarCreateView(LoginRequiredMixin, generic.CreateView):
     model = Car
     fields = "__all__"
